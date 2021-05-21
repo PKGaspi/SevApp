0fb1b0cd6dd802da718fef6c95e2c7846fee4253
Juan Gaspar García <gaxdpar@gmail.com>
2021-05-21 17:48:20 +0300

    Disabled dynamic loading by default

--- src/Main.gd
+++ src/Main.gd
@@ -1,24 +1,26 @@
 extends Control
 
+const DYNAMIC_LOAD = false
+
 const SOUNDS_PATH = "res://res/sounds"
 const SOUND_BUTTON = preload("res://src/SoundButton.tscn")
 
 onready var _buttons_container = $ScrollContainer/VBoxContainer/Buttons
 
 func _ready() -> void:
-	var dir := Directory.new()
-	var err = dir.open(SOUNDS_PATH)
-	if err == OK:
-		dir.list_dir_begin()
-		var file := dir.get_next()
-		while file != "":
-			if dir.file_exists(file):
-				var res := ResourceLoader.load("%s/%s" % [SOUNDS_PATH, file], "SoundData")
-				print(res)
-				add_button(res)
-			file = dir.get_next()
-	else:
-		printerr("Error opening sounds path. ", dir.open(SOUNDS_PATH))
+	if DYNAMIC_LOAD: 	
+		var dir := Directory.new()
+		var err = dir.open(SOUNDS_PATH)
+		if err == OK:
+			dir.list_dir_begin()
+			var file := dir.get_next()
+			while file != "":
+				if dir.file_exists(file):
+					var res := ResourceLoader.load("%s/%s" % [SOUNDS_PATH, file], "SoundData")
+					add_button(res)
+				file = dir.get_next()
+		else:
+			printerr("Error opening sounds path. ", dir.open(SOUNDS_PATH))
 
 
 
