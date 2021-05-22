8b0dbf7ae59a73bffe8fca01bb9df6a34d051f1d
Juan Gaspar García <gaxdpar@gmail.com>
2021-05-22 17:08:06 +0300

    Added hide/show language

--- src/LanguageSectionContainer.gd
+++ src/LanguageSectionContainer.gd
@@ -2,6 +2,7 @@ extends VBoxContainer
 
 onready var _title_node := $MarginContainer/MarginContainer/TitleLabel
 onready var _buttons := $Buttons
+onready var _hide_button_node := $MarginContainer/HideButton
 
 
 func set_title(value: String) -> void:
@@ -9,3 +10,8 @@ func set_title(value: String) -> void:
 
 func add_button(button: Control) -> void:
 	_buttons.add_child(button)
+
+
+func _on_HideButton_toggled(button_pressed: bool) -> void:
+	_buttons.visible = !button_pressed
+	_hide_button_node.text = "Show" if button_pressed else "Hide"
